244b731091e7dd596cef702f3a71a2763c294b3f
zamorai <35697970+zamorai@users.noreply.github.com>
2020-09-11 00:48:36 -0400

    Completed search bar and api youtube key

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyADZ-D1iCyTIPMz9zoEW_IFVC0MSmxtK5I';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY
+  }
+});
